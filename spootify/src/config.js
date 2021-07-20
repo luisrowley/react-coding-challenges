be03f26391aa019cbeffbb4f111a9c25fbd34870
Luis De Benito <louisdebenito@gmail.com>
2021-07-20 09:25:08 +0200

    added config test.

--- spootify/src/config.js
+++ spootify/src/config.js
@@ -2,7 +2,7 @@ export default {
   api: {
     baseUrl: 'https://api.spotify.com/v1',
     authUrl: 'https://accounts.spotify.com/api/token',
-    clientId: '',
+    clientId: 'some change',
     clientSecret: ''
   }
 }
